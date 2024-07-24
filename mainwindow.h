195f779c51e25407e98c78ca3f92f36b03d7f729
EKKO <ekko0908@outlook.com>
2024-07-24 21:43:08 +0800

    day01

---
+++ mainwindow.h
@@ -0,0 +1,24 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "logindialog.h"
+#include "registerdialog.h"
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+void SlotSwitchReg();
+private:
+    Ui::MainWindow *ui;
+    LoginDialog *_login_dialog;
+    RegisterDialog *_reg_dialog;
+};
+#endif // MAINWINDOW_H
